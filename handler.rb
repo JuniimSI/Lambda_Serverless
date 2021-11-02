c64c0693fa01ed97c3fbddf32af71938a5083580
JuniimSI <jrfernandesti@gmail.com>
2021-11-02 17:06:49 -0300

    Fixed error on aws

--- handler.rb
+++ handler.rb
@@ -4,10 +4,16 @@ load 'vendor/bundle/bundler/setup.rb'
 
 require 'correios-cep'
 
-def handler(event:)
+def handler(event:, context:)
   cep = event['pathParameters']['cep']
 
   address = Correios::CEP::AddressFinder.get(cep)
 
-  { statusCode: 200, body: JSON.generate(address) }
+  if address.empty?
+    { statusCode: 404, body: JSON.generate('Não encontrado o cep passado.') }
+  else
+    { statusCode: 200, body: JSON.generate(address) }
+  end
+rescue ArgumentError
+  { statusCode: 422, body: JSON.generate('Não processável o cep passado.') }
 end
